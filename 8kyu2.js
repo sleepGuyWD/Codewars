338615b9139a6d5085dd2afa8fe8ef1534fdbe33
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-09-24 09:26:39 -0700

    Daily Codewars

---
+++ 8kyu2.js
@@ -0,0 +1,9 @@
+//Volume of Cuboid
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    let volume = length * width * height
+    return volume// your code here
+  }
+}
