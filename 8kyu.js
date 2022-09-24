868571825722d3a00f5e40cefb990f372e1c151e
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-09-24 09:19:09 -0700

    Daily codewars

---
+++ 8kyu.js
@@ -0,0 +1,7 @@
+//Opposite Number
+//Very simple, given an integer or a floating-point number, find its opposite.
+function opposite(number) {
+  let result = -(number)
+  return result
+    //your code here
+}
