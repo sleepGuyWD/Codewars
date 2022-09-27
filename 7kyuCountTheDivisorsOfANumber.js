4f2c26d563424fdca504e61becefe1663b3599ee
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-09-27 06:27:57 -0700

    Daily codewars2

---
+++ 7kyuCountTheDivisorsOfANumber.js
@@ -0,0 +1,13 @@
+/*Count the number of divisors of a positive integer n.
+Random tests go up to n = 500000.
+*/
+
+function getDivisorsCnt(n){
+  let count = 0
+  for (i = 1; i < 500000; i++) {
+    if (n % i == 0) {
+    count++
+    }
+  } 
+  return count
+}
