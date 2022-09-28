7ba82b79caf3adc63b27e1471bf90d34b40a71fd
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-09-27 20:19:44 -0700

    Daily codewars3

---
+++ 8kyuGrasshopperMessiGoals.js
@@ -0,0 +1,10 @@
+/*Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+*/
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
