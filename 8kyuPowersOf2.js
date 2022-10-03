3fd309e313212fddc5cd5d3b759bf696fd3cf6a3
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-02 20:57:21 -0700

    Daily codewars5

--- 8kyuPowersOf2.js
+++ 8kyuPowersOf2.js
@@ -1,4 +1,5 @@
 //Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
 function powersOfTwo(n){
   let newArray = []
   for (i = 0; i <= n; i++) {
