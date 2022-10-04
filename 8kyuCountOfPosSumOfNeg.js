340ac4141a073cf2b92888fd7506332fb09f5ef0
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-03 21:39:38 -0700

    Daily codewars 9

---
+++ 8kyuCountOfPosSumOfNeg.js
@@ -0,0 +1,23 @@
+/*Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+*/
+function countPositivesSumNegatives(input) {
+  if (input == null || input == 0) {
+    return []
+  } else {
+    let positiveCount = 0
+    let negSum = 0
+    input.forEach ((x) => {
+      if (x > 0) {
+        positiveCount += 1
+      } else {
+        negSum += x
+      }
+    })
+   let array = [positiveCount, negSum]               
+   return array
+  }
+}
