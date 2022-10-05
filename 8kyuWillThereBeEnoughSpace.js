aa0abf705caf37a12a50af4b0a76c2a19e956ecc
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-04 20:22:21 -0700

    Daily codewars

---
+++ 8kyuWillThereBeEnoughSpace.js
@@ -0,0 +1,15 @@
+/*You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus excluding the driver.
+wait is the number of people waiting to get on to the bus excluding the driver.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+*/
+function enough(cap, on, wait) {
+  let remainder = (cap - on) - wait
+  if (remainder >= 0) {
+    return 0
+  } else {
+    return Math.abs(remainder)
+  }
+}
