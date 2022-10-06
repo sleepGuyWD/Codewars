2b83a67a44b45fe2a7d31dbb61d72b22350f781b
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-05 20:44:38 -0700

    Daily code wars 2

---
+++ 7kyuSumOfLowestPositiveInts.js
@@ -0,0 +1,6 @@
+//Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+function sumTwoSmallestNumbers(numbers) {  
+  let sorted = numbers.sort((a, b) => a-b)
+  let sum = sorted[0] + sorted[1]
+  return sum
+}
