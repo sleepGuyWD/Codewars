945dcfdae82203bb1463a5b958f07827a26b9ae5
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-13 20:42:11 -0700

    Daily Codewars 2

---
+++ 8kyuConvertANumberToString.js
@@ -0,0 +1,8 @@
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+*/
+function numberToString(num) {
+  return num.toString()
+}
