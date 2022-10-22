10268cdff9aa50b7823875a1f36f021cbedd4dc9
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-22 11:18:46 -0700

    Daily Codewars 3

---
+++ 7kyuListFiltering.js
@@ -0,0 +1,6 @@
+/*In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+*/
+function filter_list(l) {
+  let filtered = l.filter(x => typeof x == 'number')
+  return filtered
+}
