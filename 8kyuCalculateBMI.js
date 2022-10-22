99ab0506c524e3b8345fab13a25b86050eeadd8d
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-21 17:48:45 -0700

    Daily Codewars

---
+++ 8kyuCalculateBMI.js
@@ -0,0 +1,18 @@
+/*
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+function bmi(weight, height) {
+  let bmi = (weight/(height**2))
+  return bmi <= 18.5 ? "Underweight" :
+         bmi <= 25.0 ? "Normal" :
+         bmi <= 30.0 ? "Overweight" :
+         "Obese"
+}
