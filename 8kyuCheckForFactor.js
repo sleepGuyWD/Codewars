0d8fde4055ac44c94694cf863dfccb24921af900
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-10-23 09:00:17 -0700

    Daily Codewars 2

---
+++ 8kyuCheckForFactor.js
@@ -0,0 +1,7 @@
+/*This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+*/
+function checkForFactor (base, factor) {
+  return base % factor == 0 ? true : false
+}
