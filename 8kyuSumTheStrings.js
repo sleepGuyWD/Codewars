49bb5f8854d264878c13191b7dd8f4968aaa1bc6
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-11-20 21:37:33 -0800

    Daily Codewars

---
+++ 8kyuSumTheStrings.js
@@ -0,0 +1,6 @@
+/*
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):*/
+function sumStr(a,b) {
+  let sum = Number(a) + Number(b)
+  return sum.toString()
+}
