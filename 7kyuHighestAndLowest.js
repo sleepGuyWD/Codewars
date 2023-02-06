d96b11d0fb30edc2b9f66520dff241283e231a56
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-02-05 21:56:30 -0800

    Daily Codewars refactored

--- 7kyuHighestAndLowest.js
+++ 7kyuHighestAndLowest.js
@@ -1,7 +1,6 @@
 /*
 In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.*/
 function highAndLow(numbers){
-  let splitted = numbers.split(" ")
-  let sorted = splitted.sort((a, b) => a-b)
-  return `${sorted[sorted.length-1]} ${sorted[0]}`
+  let splitSort = numbers.split(" ").sort((a, b) => a-b)
+  return `${splitSort[splitSort.length-1]} ${splitSort[0]}`
  }
