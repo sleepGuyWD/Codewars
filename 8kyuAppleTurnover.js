bcbfb144152d267fa0d1366a60990e63a240380c
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-03-04 11:05:24 -0800

    Daily Codewars

---
+++ 8kyuAppleTurnover.js
@@ -0,0 +1,9 @@
+/*
+Task
+Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+Note: Input will either be a positive integer (or a string for untyped languages).
+*/
+function apple(x){
+  return x**2 > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox.`
+}
