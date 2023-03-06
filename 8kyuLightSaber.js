45c4cf305c72ba4753ca1dff865ff3ba898a50b0
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-03-05 21:02:04 -0800

    Daily Codewars

---
+++ 8kyuLightSaber.js
@@ -0,0 +1,8 @@
+/*
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+Note: your function should have a default parameter.
+*/
+function howManyLightsabersDoYouOwn(name) {
+  return name == 'Zach' ? 18 : 0
+}
