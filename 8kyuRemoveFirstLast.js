55c7e9d3dd128ffbf7dbbf2ec744c63231d2a730
Warren <warrendira@gmail.com>
2023-03-28 08:19:03 -0700

    Refactored Codewars

--- 8kyuRemoveFirstLast.js
+++ 8kyuRemoveFirstLast.js
@@ -1,11 +1,26 @@
+/*
+This is a spin off of my first kata.
+
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+*/
 function array(arr){
-  if (arr.length < 3 || arr == '') {
-    return null
-  }  else {
-    let split = arr.split(',')
-    split.pop()
-    split.shift()
-    let spaced = split.join(' ')
-    return spaced
-  }
+  let split = arr.split(',')
+  split.pop()
+  split.shift()
+  
+  return split.length === 0 ? null : split.join(' ')
+
 }
