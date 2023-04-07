b0e6c4bcaaeb0c192b612e0d854257cbec9e6f78
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-04-07 16:57:05 -0700

    Daily Codewars

---
+++ 8kyuCombineStrings.js
@@ -0,0 +1,9 @@
+/*
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+*/
+function combineNames(a, b) {
+  return `${a} ${b}`
+}
