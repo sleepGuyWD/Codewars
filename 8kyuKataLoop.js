5dd28fb7e2c286c51b81a7e598102020098c22f6
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-04-19 22:20:33 -0700

    Daily Codewars

---
+++ 8kyuKataLoop.js
@@ -0,0 +1,11 @@
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+
+function loopAdd(n) {
+  for (n = 0; n < 1000; n++) {
+    websites.push('codewars')
+  }
+}
+
+loopAdd(1000)
+
