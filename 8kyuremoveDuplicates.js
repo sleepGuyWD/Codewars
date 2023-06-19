af0090508188db41e361c7f9efc7212f24be46d2
Warren <warrendira@gmail.com>
2023-06-19 12:22:32 -0700

    Daily Codewars

---
+++ 8kyuremoveDuplicates.js
@@ -0,0 +1,8 @@
+/*
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+*/
+function distinct(a) {
+  return a.filter((item, index) => a.indexOf(item) === index )
+}
