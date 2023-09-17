0cbd7d48c45255943e2df89fac68b5e1267ad6b7
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-09-16 17:30:41 -0700

    Daily Codewars

--- 6kyumexicanwave.js
+++ 6kyumexicanwave.js
@@ -1,26 +1,27 @@
 function wave(str){
   let array = []
+  let answer = []
+  
+  //create array to start with
   for (i = 1; i <= str.length; i++) {
     array.push(str)
   }
-  let splittedArray = []
   
+  //create array of cap'd letters related to str
   let wave = array.map((el, i) => el[i].toUpperCase())
   
-  /*
-  array.forEach ((el, i) => {
-    let split = el.split('')
-    splittedArray.push(split)
-  })*/
-
   
   array.forEach((el, i) => {
-    let word = el.slice(el[i]).join(wave[i])
-    splittedArray.push(word)
+    
+    let wordSplit = el.split('')
+    
+    wordSplit.splice(i, 1, wave[i])
+
+    let joined = wordSplit.join('')
+
+    answer.push(joined)
+    
   })
   
-  
-  
-  console.log(splittedArray)
-  
+  return answer
 }
