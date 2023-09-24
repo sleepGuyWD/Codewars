2876450dc77c90aeace83b1d0963760cd122e073
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-09-23 23:59:53 -0700

    Finished Codewars solution

--- 7kyumaximumproduct.js
+++ 7kyumaximumproduct.js
@@ -8,3 +8,16 @@ Array/list numbers could be a mixture of positives, negatives also zeroes .
 
 Input >> Output Examples
 adjacentElementsProduct([1, 2, 3]); ==> return 6*/
+
+function adjacentElementsProduct(array) {
+  let productArray = []
+  
+  for (i = 1; i < array.length; i++) {
+    let prodElement = array[i] * array[i-1]
+    productArray.push(prodElement)
+  }
+  
+  let sort = productArray.sort((a, b) => a - b)
+  
+  return sort[sort.length-1]
+}
