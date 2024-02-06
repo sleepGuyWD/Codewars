38cb57bf30327bde2eb67f517b5cbcf14057588e
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2024-02-05 22:06:40 -0800

    Daily Codewars

--- 7kyudivideandconquer.js
+++ 7kyudivideandconquer.js
@@ -2,12 +2,13 @@
 Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
 
 Return as a number.*/
+
 function divCon(x){
   //declare empty arrays for both non-strings and strings
   let nonString = []
   let string = []
   
-  //loop through the parameter array with forEach Method. Loop will go through each element applying a condition of typeof, and if the type is number, then we will push the element to the nonstring array, else we will push the element to the string array.  If the type is not a number, we will convert the type into a number first.
+  //loop through the parameter array with forEach Method. Loop will go through each element applying a condition of typeof. If the type is number, then we will push the element to the nonstring array, else we will push the element to the string array.  Element will be converted into a number before pushing to the string array.
   x.forEach(el => typeof el == 'number' ? nonString.push(el) : string.push(Number(el)))
   
   //Sum up both arrays above using reduce method, then storing them into variables
